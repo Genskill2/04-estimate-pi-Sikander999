ca7f1eb28e87849276c495b7bedf698c4c8b7df2
Sikander999 <85334374+Sikander999@users.noreply.github.com>
2021-06-04 13:43:11 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,4 +23,30 @@ int main(void) {
     }
   }
 }
-
+float wallis_pi(int n)
+{
+  float s=1.0;
+  for (int i=1; i<=n; i++)
+  {
+    s=s*(4*i*i)/((4*i*i)-1);
+  }
+  return (2*s);
+}
+float wallis_pi(int n) {
+  int i;
+  float x,y,z,pi;
+  if(n==0) return 0;
+   for(i=1;i<=n;i++)
+   {
+     x=4*i*i;
+     y=x-1;
+     float(z)=(x)/(y);
+     if(i==1){
+        pi=z;}
+     else
+     {pi=pi*z;}
+   }
+  pi*2;
+  return pi;
+}
+      
